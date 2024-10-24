af05a637a982073b2ccc1ad24a6b686dcb1d3cb0
Johannes Skuterud <johann1301s@icloud.com>
2024-10-24 16:01:36 +0200

    wip

--- components/editor.tsx
+++ components/editor.tsx
@@ -14,24 +14,33 @@ export const Editor = (props: Props) => {
         apiKey={'5mwypg9c08ih4fcpubmb57cavmibsx4ws639q0m85gy6b6hg'}
         value={props.value}
         init={{
-            plugins: [
-            'flite',
-            'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-            'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
-            ],
-            toolbar: 'flitefind flitelocate fliteinsert flitetrack fliteedit undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat', // FLITE toolbar buttons
-            tinycomments_mode: 'embedded',
-            tinycomments_author: 'Author name',
-            mergetags_list: [
-            { value: 'First.Name', title: 'First Name' },
-            { value: 'Email', title: 'Email' },
-            ],
-            height: 500,
-            menubar: true, 
-            flite_options: {
-            track_changes: true
+            width: '1000px',
+            plugins: ['tinycomments'],
+            toolbar: 'bold italic | addcomment showcomments',
+            tinycomments_mode: 'callback',
+            tinycomments_author: 'Ola Nordamnn',
+            tinycomments_create: (event: any) => {
+                console.log(event)
             },
-        }}
+            tinycomments_reply: (event: any) => {
+                console.log(event)
+            },
+            tinycomments_lookup: (event: any) => {
+                console.log(event)
+            },
+            tinycomments_delete: (event: any) => {
+                console.log(event)
+            },
+            tinycomments_delete_all: (event: any) => {
+                console.log(event)
+            },
+            tinycomments_delete_comment: (event: any) => {
+                console.log(event)
+            },
+            tinycomments_edit_comment: (event: any) => {
+                console.log(event)
+            }
+          }}
         onEditorChange={props.onChange}
     />
   );
