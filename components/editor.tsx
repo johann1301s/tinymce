fa78645ef8b020207129553f7d9620798156a90f
Johannes Skuterud <johann1301s@icloud.com>
2024-10-25 10:17:45 +0200

    wip

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { Editor as TEditor } from '@tinymce/tinymce-react';
 
@@ -9,6 +9,23 @@ type Props = {
 
 export const Editor = (props: Props) => {
 
+    const conversations = {
+        'id-1': {
+            uid: 'uid-1',
+            comments: [
+                {
+                    uid: 'uid-2',
+                    author: 'author-1',
+                    authorName: 'Kari Nordmann',
+                    authorAvatar: 'av-1',
+                    content: 'Hey, look! A comment!',
+                    createdAt: '2024-10-25T00:00:00Z',
+                    modifiedAt: '2024-10-25T00:00:00Z'
+                },
+            ]
+        },
+    }
+      
   return (
     <TEditor
         apiKey={'5mwypg9c08ih4fcpubmb57cavmibsx4ws639q0m85gy6b6hg'}
@@ -20,31 +37,34 @@ export const Editor = (props: Props) => {
                 'bold italic',
                 'addcomment showcomments'
             ],
+            skin: 'COSTUM',
+            content_css: "/",
+            skin_url: '/',
             tinycomments_mode: 'callback',
-            tinycomments_author: 'Ola Nordamnn',
-            tinycomments_fetch: (event: any) => {
-                console.log(event)
+            tinycomments_author: 'Ola Nordmann',
+            tinycomments_fetch: (event: any, done: any) => {
+                done({ conversations });
             },
             tinycomments_create: (event: any) => {
-                console.log(event)
+                console.log(event);
             },
             tinycomments_reply: (event: any) => {
-                console.log(event)
+                console.log(event);
             },
             tinycomments_lookup: (event: any) => {
-                console.log(event)
+                console.log(event);
             },
             tinycomments_delete: (event: any) => {
-                console.log(event)
+                console.log(event);
             },
             tinycomments_delete_all: (event: any) => {
-                console.log(event)
+                console.log(event);
             },
             tinycomments_delete_comment: (event: any) => {
-                console.log(event)
+                console.log(event);
             },
             tinycomments_edit_comment: (event: any) => {
-                console.log(event)
+                console.log(event);
             }
           }}
         onEditorChange={props.onChange}
