a6003890236b202683da6b9cb51f552c27f973c4
Johannes Skuterud <johann1301s@icloud.com>
2024-11-01 13:37:02 +0100

    wip

--- components/editor.tsx
+++ components/editor.tsx
@@ -95,7 +95,7 @@ export const Editor = (props: Props) => {
                     content_css: "/",
                     skin_url: '/',
                     tinycomments_mode: 'embedded',
-                    tinycomments_author: users.find(({id}) => props.activeUserId == id)?.name,
+                    tinycomments_author: 'User', // users.find(({id}) => props.activeUserId == id)?.name,
                     tinycomments_fetch: (event: any, done: any) => {
                         done({ conversations });
                     },
