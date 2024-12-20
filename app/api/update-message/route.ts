6b94080fd96a9e438bc2080efacf0a1da724df54
Johannes Skuterud <johann1301s@icloud.com>
2024-12-20 09:16:43 +0100

    wip

---
+++ app/api/update-message/route.ts
@@ -0,0 +1,32 @@
+import { getDbConnection } from '@/db/client';
+import { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const { searchParams } = new URL(req.url);
+    const query = `
+      UPDATE MessagesPoc
+      SET Message = '${searchParams.get('message') || '-'}'
+      WHERE EmployeeID = 1;
+    `
+
+    const pool = await getDbConnection();
+    const result = await pool.request().query(query);
+
+    const response = NextResponse.json({
+      status: 200,
+      foo: result.recordset,
+    });
+    response.headers.set("Content-Type", "application/json");
+    return response;
+
+  } catch (error) {
+    const response = NextResponse.json({
+      status: 500,
+      error
+    });
+    response.headers.set("Content-Type", "application/json");
+    return response;
+  }
+}
