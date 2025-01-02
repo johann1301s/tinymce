1baefbf648be39786781a33b39d34c8b65bb530b
Johannes Skuterud <johann1301s@icloud.com>
2025-01-02 14:02:19 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
       }
     }
 
-    fetchFirstMessage()
+    // fetchFirstMessage()
   }, [])
 
   const handleSubmit = async () => {
--- components/editor.tsx
+++ components/editor.tsx
@@ -1,4 +1,3 @@
-'use client'
 
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Editor as TEditor } from '@tinymce/tinymce-react';
