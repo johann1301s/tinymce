982326fe7036c1dfc150c952f87346f438a01d32
Johannes Skuterud <johann1301s@icloud.com>
2025-01-22 16:13:43 +0100

    wip

--- editor/editor.tsx
+++ editor/editor.tsx
@@ -31,10 +31,10 @@ export const Editor = (props: Props) => {
 			const lance = event.lance,
 				ann = lance.getAnnotations();
 			ann.addUsers(props.users);
-			ann.setUserId(props.users[0].id);
+			ann.setUserId(props.user.id);
 			setLanceGlobals(lance.App);
 		});
-	}, [props.users])
+	}, [props.users, props.user])
 
     useEffect(() => {
         flite?.setUserInfo(props.user.id)
@@ -47,6 +47,7 @@ export const Editor = (props: Props) => {
                     apiKey={'tpwemofiiae8simzlmhkevt82ywprtc8szdc80usdo8xdy33'}
                     value={props.value}
                     onEditorChange={props.onChange}
+                    plugins={'wordcount'}
                     toolbar={[
                         'bold italic underline strikethrough | redo undo | removeformat | alignleft aligncenter alignright alignjustify',
                         'lance',
